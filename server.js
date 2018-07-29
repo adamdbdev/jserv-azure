a74a8395f69a291149f41cbe0aab17ee7f42dc1b
adamdbdev <41781341+adamdbdev@users.noreply.github.com>
2018-07-29 15:43:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const express = require('express');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+const app = express();
 
-server.listen(port);
+app.use((req, res, next) => {
+  console.log('REQUEST: ', req)
+  next()
+})
+
+app.use('/api', jsonServer.router(path.join(__dirname, '..', 'server', 'db.json')))
